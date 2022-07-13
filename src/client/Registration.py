5077a08bbd522819a3ab205c7b4d1423bee2a666
Aysan-n <103320860+Aysan-n@users.noreply.github.com>
2022-07-13 23:59:12 +0430

    Update Registration.py

--- src/client/Registration.py
+++ src/client/Registration.py
@@ -1,10 +1,10 @@
 import rsa
 
 
-def initiate_registration(pub_key, first_name, last_name, username, password):
-    send_to_server(rsa.encrypt(build_message(first_name, last_name
-                                             , username, password), pub_key))
+def initiate_registration(messaging, pub_key, first_name, last_name, username, password):
+    cipher = rsa.encrypt(build_message(first_name, last_name, username, password).encode(), pub_key).hex()
+    messaging.send_message({'message_type': 'registration', 'cipher': cipher})
 
 
 def build_message(first_name, last_name, username, password):
-    return "REGISTER " + first_name + " " + last_name + " " + username + " " + password
+    return first_name + " " + last_name + " " + username + " " + password
