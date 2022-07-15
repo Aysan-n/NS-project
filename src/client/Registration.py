b21559ce1cd9e684f9473a07066854f92876d7ea
aliakbarrezaei <101210449+aliakbarrezaei@users.noreply.github.com>
2022-07-15 17:50:26 +0430

    Update Registration.py

--- src/client/Registration.py
+++ src/client/Registration.py
@@ -12,3 +12,22 @@ def initiate_registration(messaging, pub_key, first_name, last_name, username, p
 
 def build_message(first_name, last_name, username, password):
     return first_name + " " + last_name + " " + username + " " + password
+
+
+
+
+import rsa
+
+
+def initiate_registration(messaging, pub_key, first_name, last_name, username, password):
+    cipher = rsa.encrypt(build_message(first_name, last_name, username, password).encode(), pub_key).hex()
+    messaging.send_message({'message_type': 'registration', 'cipher': cipher})
+    reply = messaging.receive()
+    print(reply)
+    if reply['status'] == 'failed':
+        print("Registration failed. Try again!")
+
+
+def build_message(first_name, last_name, username, password):
+    return first_name + " " + last_name + " " + username + " " + password
+
